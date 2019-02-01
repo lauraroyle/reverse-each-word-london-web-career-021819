5fd2de8299cd0c1f4ea3d902b8c83f6b8a899915
Laura Royle <lauraamie.royle@gmail.com>
2019-02-01 10:50:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(sentence1)
   new_arr = sentence1.split
 
-    new_arr = new_arr.collect
-      {|word| word = word.reverse }
+    new_arr = new_arr.collect {|word| 
+      word = word.reverse 
+    }
         new_arr.join(" ")
       end
 end
